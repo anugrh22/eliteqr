ac42ceb34f0ca12ec062895a846e7ea1505b34f7
anugrh22 <discord2312dis@gmail.com>
2026-06-07 16:06:00 +0530

    Backend setup

--- backend/main.py
+++ backend/main.py
@@ -5,3 +5,10 @@ app = FastAPI()
 @app.get("/")
 def home():
     return {"message": "Backend running"}
+
+@app.post("/create-qr")
+def create_qr(data: dict):
+    return {
+        "url": data["url"],
+        "status": "received"
+    }
